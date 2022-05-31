40d6ace9807ff5e29e7f2c2bd5c07d4c265b470e
Cod3lta <valentin.pretat@gmail.com>
2022-05-31 23:05:41 +0000

    added sass preprocessor

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,23 @@ module.exports = {
             },
         },
     },
+    daisyui: {
+        themes: [
+            {
+                RedCraft: {
+                    "primary": "#C24F4F",
+                    "secondary": "#8F3B3B",
+                    "accent": "#7A8185",
+                    "neutral": "#F2F2F3",
+                    "base-100": "#2B2D2F",
+                    "info": "#4F96C2",
+                    "success": "#4FC270",
+                    "warning": "#FFC547",
+                    "error": "#9558C7",
+                },
+            },
+        ],
+    },
 
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require("daisyui")],
 };
