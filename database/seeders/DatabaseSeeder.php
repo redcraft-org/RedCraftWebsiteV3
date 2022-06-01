64ba826500781d41f9d006f75e23388670905378
Harrison <35530362+Nano112@users.noreply.github.com>
2022-06-01 00:46:30 +0000

    created category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             LanguageSeeder::class,
             ProviderSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
