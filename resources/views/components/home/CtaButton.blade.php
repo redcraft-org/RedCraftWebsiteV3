c5b92f97e1891740c9f7b58221e6b3baf2a5146a
Cod3lta <valentin.pretat@gmail.com>
2022-06-24 00:29:02 +0000

    CtaButton blade component

---
+++ resources/views/components/home/CtaButton.blade.php
@@ -0,0 +1,6 @@
+<button class="btn btn-lg {{ $type }}">
+    <div>
+        <h5>{{ $text1 }}</h5>
+        <div class="text-sm">{{ $text2 }}</div>
+    </div>
+</button>
--- resources/views/components/home/header.blade.php
+++ resources/views/components/home/header.blade.php
@@ -3,7 +3,7 @@
 
         {{-- RedCraft animated logo --}}
         <div class="sm:w-1/2 h-fit flex justify-center my-auto">
-            <img src="{{ asset('images/home/rc-logo-animated.gif'); }}">
+            <img src="{{ asset('images/home/rc-logo-animated.gif') }}">
         </div>
 
         <div class="sm:w-1/2 flex flex-col justify-center">
@@ -13,7 +13,19 @@
         </div>
     </div>
     <div class="flex flex-wrap justify-center gap-3">
-        <x-jet-button type="btn-primary">Call to action IP</x-jet-button>
-        <x-jet-button type="btn-secondary">Call to action Discord</x-jet-button>
+        <x-home.CtaButton
+            :type="'btn-primary'"
+            :text1="'Rejoindre le serveur'"
+            :text2="'69 joueurs en ligne'"
+            :triggerText="'Adresse IP copiée !'">
+                Call to action IP
+        </x-home.CtaButton>
+        <x-home.CtaButton
+            :type="'btn-secondary'"
+            :text1="'Rejoindre le Discord'"
+            :text2="'420 joueurs en ligne'"
+            :href="'https://discord.gg/xkWE4uJ'">
+                Call to action IP
+        </x-home.CtaButton>
     </div>
 </x-section>
