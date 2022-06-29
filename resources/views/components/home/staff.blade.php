dbff86dab0036f98f34112ff21e25dd59cdef159
Cod3lta <valentin.pretat@gmail.com>
2022-06-29 01:18:48 +0000

    various tweaks

--- resources/views/components/home/staff.blade.php
+++ resources/views/components/home/staff.blade.php
@@ -1,6 +1,17 @@
 @php
 $staffMembers = [
     [
+        'displayName' => 'Nano',
+        'uuid' => '45418653-fadb-4e6d-8dcc-8c79b90ec527',
+        'socials' => [
+            [
+                'socialName' => 'steam',
+                'logo' => 'fa-steam',
+                'url' => 'https://google.com',
+            ],
+        ],
+    ],
+    [
         'displayName' => 'Lululombard',
         'uuid' => '15f7b6b0-d787-48e2-b54b-0336e17f6316',
         'socials' => [
@@ -22,17 +33,6 @@ $staffMembers = [
             [
                 'socialName' => 'youtube',
                 'logo' => 'fa-youtube',
-                'url' => 'https://google.com',
-            ],
-        ],
-    ],
-    [
-        'displayName' => 'Nano',
-        'uuid' => '45418653-fadb-4e6d-8dcc-8c79b90ec527',
-        'socials' => [
-            [
-                'socialName' => 'steam',
-                'logo' => 'fa-steam',
                 'url' => 'https://google.com',
             ],
         ],
@@ -76,11 +76,11 @@ $staffMembers = [
 <x-section id="staff" title="Staff">
     <div class="flex justify-evenly flex-wrap gap-y-8 gap-x-4">
         @foreach ($staffMembers as $staffMember)
-            <div class="mx-4 flex flex-col gap-y-4">
+            <div class="flex w-32 mx-4 flex-col gap-y-2">
                 <img class="border-light border-4 rounded-md w-24 m-auto" alt="Membre du staff : {{ $staffMember['displayName'] }}"
                     src="https://mc-heads.net/avatar/{{ $staffMember['uuid'] }}">
-                <p class="flex justify-evenly text-xl font-bold">{{ $staffMember['displayName'] }}</p>
-                <div class="flex justify-around flex-wrap">
+                <p class="flex justify-evenly text-xl font-bold text-center">{{ $staffMember['displayName'] }}</p>
+                <div class="flex justify-evenly flex-wrap">
                     @foreach ($staffMember['socials'] as $social)
                         <a class="transition ease-in-out delay-150 hover:text-primary duration-300" href={{ $social['url'] }} target="_blank">
                             <i class="fa-brands {{ $social['logo'] }} "></i>
