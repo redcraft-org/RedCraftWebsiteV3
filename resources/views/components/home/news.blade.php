6fb177e4d61982aed3842acd29cf5066d18e0c2d
Cod3lta <valentin.pretat@gmail.com>
2022-06-30 20:04:09 +0000

    cards test

--- resources/views/components/home/news.blade.php
+++ resources/views/components/home/news.blade.php
@@ -1,4 +1,37 @@
 <x-section id="news" title="Nouveautés">
-    les news ici 👀
-
+    <div class="flex flex-row gap-8">
+        <div class="card w-96 bg-neutral text-base-100 shadow-xl">
+            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+        <div class="card w-96 bg-neutral text-base-100 shadow-xl">
+            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+        <div class="card w-96 bg-neutral text-base-100 shadow-xl">
+            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mt-8 text-right">
+        <a href="#"><i class="fa-solid fa-arrow-right"></i> Voir tous les articles</a>
+    </div>
 </x-section>
