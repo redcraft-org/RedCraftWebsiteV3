0b4c0e90a77031eb4561ec7267149db82b4837e5
Cod3lta <valentin.pretat@gmail.com>
2022-08-11 12:09:31 +0000

    better desktop-mobile transition

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,4 +1,8 @@
 @php
+
+// Decide at what point the mobile menu should be displayed
+$mobileSizeTrigger = 'sm';
+
 // TODO translate navigation menu names
 $links = [
     [
@@ -14,66 +18,55 @@ $links = [
         'route' => 'stats',
     ],
     [
-        'name' => 'Stats',
-        'route' => 'stats',
-    ],
-    [
-        'name' => 'Stats',
-        'route' => 'stats',
-    ],
-    [
         'name' => 'Rules',
-        'route' => 'rules',
-    ],
-    [
-        'name' => 'Rulesasdasd',
         'route' => 'rules',
     ],
 ];
 @endphp
 
-
-
-<nav x-data="{ open: false }">
-    <div class="mx-auto px-8 container md:max-w-screen-lg navbar">
-        <div class="flex py-1 justify-between">
-            <div class="hidden sm:flex">
-                <x-jet-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                    <img id="rc-logo-homepage" src="{{ asset('images/inline_org_color.png') }}">
+<nav x-data="{ mobileOpen: false }" class="mx-auto px-8 container md:max-w-screen-lg navbar">
+    <div class="flex py-1 justify-between">
+        <div>
+            <x-jet-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                <img id="rc-logo-homepage" src="{{ asset('images/inline_org_color.png') }}">
+            </x-jet-nav-link>
+        </div>
+        <div class="space-x-8 px-4 hidden <?=$mobileSizeTrigger?>:flex">
+            @foreach ($links as $link)
+                <x-jet-nav-link :href="route($link['route'])" :active="request()->routeIs($link['route'])">
+                    {{ $link['name'] }}
                 </x-jet-nav-link>
-            </div>
-            <div class="hidden space-x-8 px-4 sm:flex">
-                @foreach ($links as $link)
-                    <x-jet-nav-link :href="route($link['route'])" :active="request()->routeIs($link['route'])">
-                        {{ $link['name'] }}
-                    </x-jet-nav-link>
-                @endforeach
-            </div>
+            @endforeach
+        </div>
 
-            <x-navbar-dropdown/>
+        <x-navbar-dropdown />
 
-            <!-- Hamburger -->
-            <div class="flex items-center -mr-2 sm:hidden">
-                <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 text-gray-400 transition rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
-                    <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
+        <!-- Hamburger -->
+        <div class="items-center -mr-2 flex <?=$mobileSizeTrigger?>:hidden">
+            <button @click="mobileOpen = ! mobileOpen"
+                class="inline-flex items-center justify-center p-2 text-gray-400 transition rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
+                <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path :class="{ 'hidden': mobileOpen, 'inline-flex': !mobileOpen }" class="inline-flex"
+                        stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    <path :class="{ 'hidden': !mobileOpen, 'inline-flex': mobileOpen }" class="hidden"
+                        stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
-    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
+
+    <!-- Mobile Menu -->
+    <div :class="{ 'block': mobileOpen, 'hidden': !mobileOpen }" class="hidden <?=$mobileSizeTrigger?>:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+            {{-- <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
-            </x-jet-responsive-nav-link>
+            </x-jet-responsive-nav-link> --}}
+            @foreach ($links as $link)
+                <x-jet-responsive-nav-link :href="route($link['route'])" :active="request()->routeIs($link['route'])">
+                    {{ $link['name'] }}
+                </x-jet-responsive-nav-link>
+            @endforeach
         </div>
 
     </div>
