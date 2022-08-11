672087f0f61024990aac96606d2e67c3758c7020
Cod3lta <valentin.pretat@gmail.com>
2022-08-11 12:28:10 +0000

    fullscreen mobile menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -24,14 +24,14 @@ $links = [
 ];
 @endphp
 
-<nav x-data="{ mobileOpen: false }" class="mx-auto px-8 container md:max-w-screen-lg navbar">
+<nav x-data="{ mobileOpen: false }" class="mx-auto px-8 block border-none min-h-0 relative container md:max-w-screen-lg navbar">
     <div class="flex py-1 justify-between">
         <div>
             <x-jet-nav-link :href="route('home')" :active="request()->routeIs('home')">
                 <img id="rc-logo-homepage" src="{{ asset('images/inline_org_color.png') }}">
             </x-jet-nav-link>
         </div>
-        <div class="space-x-8 px-4 hidden <?=$mobileSizeTrigger?>:flex">
+        <div class="space-x-8 px-4 hidden <?= $mobileSizeTrigger ?>:flex">
             @foreach ($links as $link)
                 <x-jet-nav-link :href="route($link['route'])" :active="request()->routeIs($link['route'])">
                     {{ $link['name'] }}
@@ -39,10 +39,10 @@ $links = [
             @endforeach
         </div>
 
-        <x-navbar-dropdown />
+        {{-- <x-navbar-dropdown /> --}}
 
         <!-- Hamburger -->
-        <div class="items-center -mr-2 flex <?=$mobileSizeTrigger?>:hidden">
+        <div class="items-center -mr-2 flex <?= $mobileSizeTrigger ?>:hidden">
             <button @click="mobileOpen = ! mobileOpen"
                 class="inline-flex items-center justify-center p-2 text-gray-400 transition rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
                 <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -57,11 +57,10 @@ $links = [
 
 
     <!-- Mobile Menu -->
-    <div :class="{ 'block': mobileOpen, 'hidden': !mobileOpen }" class="hidden <?=$mobileSizeTrigger?>:hidden">
+    <div id="mobile-menu-content" x-show="mobileOpen"
+        class="absolute w-full left-0 top-full z-10 items-center justify-center flex <?= $mobileSizeTrigger ?>:hidden"
+        x-transition>
         <div class="pt-2 pb-3 space-y-1">
-            {{-- <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-jet-responsive-nav-link> --}}
             @foreach ($links as $link)
                 <x-jet-responsive-nav-link :href="route($link['route'])" :active="request()->routeIs($link['route'])">
                     {{ $link['name'] }}
