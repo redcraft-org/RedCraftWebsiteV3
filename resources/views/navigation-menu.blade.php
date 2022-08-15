07c564fd27277c294b899956c4566b836b46a261
Cod3lta <valentin.pretat@gmail.com>
2022-08-15 22:01:34 +0000

    small tweaks

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -59,7 +59,7 @@ $links = [
 
     <!-- Mobile Menu -->
     <div id="mobile-menu-content" x-show="mobileOpen" x-cloak
-        class="bg-base-200/90 backdrop-blur-sm drop-shadow-2xl fixed w-1/2 rounded-md right-8 top-4 z-10 py-4 flex <?= $mobileSizeTrigger ?>:hidden"
+        class="bg-base-200/90 backdrop-blur-lg drop-shadow-2xl fixed w-1/2 rounded-md right-6 top-2 z-10 py-4 flex <?= $mobileSizeTrigger ?>:hidden"
         x-transition:enter="transition out-expo duration-100" x-transition:enter-start="opacity-0 scale-90 translate-x-3.5 -translate-y-3.5"
         x-transition:enter-end="opacity-100 scale-100 translate-x-0 translate-y-0" x-transition:leave="transition in-expo duration-100"
         x-transition:leave-start="opacity-100 scale-100 translate-x-0 translate-y-0" x-transition:leave-end="opacity-0 scale-90 translate-x-3.5 -translate-y-3.5">
