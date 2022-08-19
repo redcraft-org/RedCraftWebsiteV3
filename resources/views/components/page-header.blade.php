2536887da6b8f8703cb613f41ae1192e2fd5ae18
Cod3lta <valentin.pretat@gmail.com>
2022-08-19 21:44:06 +0000

    404 page

---
+++ resources/views/components/page-header.blade.php
@@ -0,0 +1,7 @@
+<x-section class="page-header">
+    <div class="flex justify-center">
+        <h1 class="title text-center">
+            {{ $attributes['section-title'] }}
+        </h1>
+    </div>
+</x-section>
