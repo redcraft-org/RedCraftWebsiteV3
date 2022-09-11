92e73ac35ec59e6d072805ea42cce0a870128de5
Cod3lta <valentin.pretat@gmail.com>
2022-09-11 19:15:26 +0000

    wave on footer using the section component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -11,29 +11,28 @@ $links = [
     [
         'name' => 'Contact',
         'route' => 'contact',
-],
+    ],
 ];
 @endphp
 
-<footer class="section flex flex-col py-24 bg-base-200">
-    <div class="mx-auto px-8 container md:max-w-screen-lg">
-        <div class="flex flex-row">
-            <div class="w-1/2 flex items-center">
-                <x-jet-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                    <img id="rc-logo-homepage" src="{{ asset('images/inline_org_color.png') }}">
-                </x-jet-nav-link>
-            </div>
-            <div class="shrink-0 w-1/2 flex flex-col">
-                <p class="pb-4">Une toute nouvelle expérience Minecraft multilingue avec une infrastructure open source de serveurs
-                    créatif et survie soigneusement conçus.</p>
-                <div class="flex justify-between">
-                    @foreach ($links as $link)
-                        <x-jet-nav-link :href="route($link['route'])" :active="request()->routeIs($link['route'])">
-                            {{ $link['name'] }}
-                        </x-jet-nav-link>
-                    @endforeach
-                </div>
+<x-section class="pb-32" bg="bg-base-200" text="text-light" wave-bg="fill-base-200" wave-id="6">
+    <div class="flex md:flex-row flex-col">
+        <div class="md:w-1/2 flex items-center mx-auto">
+            <x-jet-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                <img id="rc-logo-homepage" src="{{ asset('images/inline_org_color.png') }}">
+            </x-jet-nav-link>
+        </div>
+        <div class="md:w-1/2 flex flex-col">
+            <p class="pb-4">Une toute nouvelle expérience Minecraft multilingue avec une infrastructure open source de
+                serveurs
+                créatif et survie soigneusement conçus.</p>
+            <div class="flex justify-between">
+                @foreach ($links as $link)
+                    <x-jet-nav-link :href="route($link['route'])" :active="request()->routeIs($link['route'])">
+                        {{ $link['name'] }}
+                    </x-jet-nav-link>
+                @endforeach
             </div>
         </div>
     </div>
-</footer>
+</x-section>
