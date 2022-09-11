c221ed886a822f9e2b446450a6408e465dd5ac44
Cod3lta <valentin.pretat@gmail.com>
2022-09-11 16:55:15 +0000

    added links and changed style

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -15,22 +15,24 @@ $links = [
 ];
 @endphp
 
-<footer class="section flex flex-col py-12">
+<footer class="section flex flex-col py-24 bg-base-200">
     <div class="mx-auto px-8 container md:max-w-screen-lg">
         <div class="flex flex-row">
-            <div>
+            <div class="w-1/2 flex items-center">
                 <x-jet-nav-link :href="route('home')" :active="request()->routeIs('home')">
                     <img id="rc-logo-homepage" src="{{ asset('images/inline_org_color.png') }}">
                 </x-jet-nav-link>
-                <p>Une toute nouvelle expérience Minecraft multilingue avec une infrastructure open source de serveurs
-                    créatif et survie soigneusement conçus.</p>
             </div>
-            <div>
-                @foreach ($links as $link)
-                    <x-jet-nav-link :href="route($link['route'])" :active="request()->routeIs($link['route'])">
-                        {{ $link['name'] }}
-                    </x-jet-nav-link>
-                @endforeach
+            <div class="shrink-0 w-1/2 flex flex-col">
+                <p class="pb-4">Une toute nouvelle expérience Minecraft multilingue avec une infrastructure open source de serveurs
+                    créatif et survie soigneusement conçus.</p>
+                <div class="flex justify-between">
+                    @foreach ($links as $link)
+                        <x-jet-nav-link :href="route($link['route'])" :active="request()->routeIs($link['route'])">
+                            {{ $link['name'] }}
+                        </x-jet-nav-link>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
