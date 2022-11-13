995edc9f2f3cbbf6d2fc54d3018d2b76d15fb2e3
Harrison <35530362+Nano112@users.noreply.github.com>
2022-11-13 14:42:54 +0000

    moved code to js

--- resources/views/components/navbar/language-dropdown.blade.php
+++ resources/views/components/navbar/language-dropdown.blade.php
@@ -1,27 +1,5 @@
-<?php
-
-function country2flag(string $countryCode): string
-{
-    return (string) preg_replace_callback('/./', static fn(array $letter) => mb_chr((ord($letter[0]) % 32) + 0x1f1e5), language2country($countryCode));
-}
-
-function language2country(string $languageCode): string
-{
-    $array = [
-        'fr' => 'fr',
-        'en' => 'gb',
-        'de' => 'de',
-        'es' => 'es',
-        'it' => 'it',
-    ];
-    return $array[$languageCode] ?? '__';
-}
-
-?>
-
-<!-- Language selector dropdown -->
 <div id="language-dropdown" x-show="languageOpen" x-cloak x-data="{ ...languageSwitcher }"
-    class="bg-base-200/90 backdrop-blur-lg drop-shadow-lg absolute w-1/2 max-w-xs rounded-md right-6 top-2 z-10 flex"
+    class="absolute z-10 flex w-1/2 max-w-xs rounded-md bg-base-200/90 backdrop-blur-lg drop-shadow-lg right-6 top-2"
     x-transition:enter="transition out-expo duration-100"
     x-transition:enter-start="opacity-0 scale-90 translate-x-3.5 -translate-y-3"
     x-transition:enter-end="opacity-100 scale-100 translate-x-0 translate-y-0"
@@ -30,9 +8,9 @@ function language2country(string $languageCode): string
     x-transition:leave-end="opacity-0 scale-90 translate-x-3.5 -translate-y-3">
     <div class="w-full">
         @foreach ($langs as $key => $lang)
-            <div class="text-white hover:text-white no-underline block px-4 py-4 cursor-pointer"
+            <div class="block px-4 py-4 text-white no-underline cursor-pointer hover:text-white"
                 @click="setLanguage('{{ $key }}')">
-                {{ country2flag($key) }} <span class="flag"></span>{{ $lang }}
+                <div x-text=countryCodeToFlag(languageCodeToCountryCode('{{ $key }}'))></div><span class="flag"></span>{{ $lang }}
             </div>
             @if (!$loop->last)
                 <hr class="text-base-100">
@@ -52,6 +30,21 @@ function language2country(string $languageCode): string
                     document.cookie = 'language' + "=" + (value || "") + expires + "; path=/";
 
                     window.location.reload();
+                },
+                languageCodeToCountryCode(languageCode) {
+                    const array = {
+                        'fr': 'fr',
+                        'en': 'gb',
+                        'de': 'de',
+                        'es': 'es',
+                        'it': 'it',
+                    };
+                    return array[languageCode] ?? '__';
+                },
+                countryCodeToFlag(code) {
+                    return code
+                        .toUpperCase()
+                        .replace(/./g, char => String.fromCodePoint(char.charCodeAt(0) + 127397));
                 },
             }
         </script>
