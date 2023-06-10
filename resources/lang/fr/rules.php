359cedddfcc7bf2bbaada904f83a4f5aa43c0806
Harrison <35530362+Nano112@users.noreply.github.com>
2023-06-11 00:19:22 +0100

    initial rull mockup - missing table

--- resources/lang/fr/rules.php
+++ resources/lang/fr/rules.php
@@ -1,80 +1,104 @@
 <?php
 
 return [
-    "title" => "Règles",
+    'title' => 'Règles',
 
-    "code_of_conduct" => [
-        "title" => "Code de conduite",
-        "1" => [
-            "title" => "Se respecter",
-            "description" => "Le respect est la base pour avoir une atmosphère saine et constructive. N'oublions pas que tout le monde n'a pas le même niveau de connaissances. Respectons ceux qui en ont moins que nous et encourageons leur curiosité. 🤝",
+    'code_of_conduct' => [
+        'title' => 'Code de conduite',
+        '1' => [
+            'title' => 'Se respecter',
+            'description' => "Le respect est la base pour avoir une atmosphère saine et constructive. N'oublions pas que tout le monde n'a pas le même niveau de connaissances. Respectons ceux qui en ont moins que nous et encourageons leur curiosité. 🤝",
         ],
-        "2" => [
-            "title" => "S'entraider",
-            "description" => "RedCraft.org a parmi ses objectifs la transmission de connaissances quelles qu'elles soient. Le partage est un pilier pour avoir une communauté soudée et active. 💪",
+        '2' => [
+            'title' => "S'entraider",
+            'description' => "RedCraft.org a parmi ses objectifs la transmission de connaissances quelles qu'elles soient. Le partage est un pilier pour avoir une communauté soudée et active. 💪",
         ],
-        "3" => [
-            "title" => "S'amuser",
-            "description" => "Après tout, nous sommes tous en train de jouer à un jeu vidéo, alors essayons de passer du bon temps ensemble pour avoir une expérience en ligne inoubliable ! 😉",
+        '3' => [
+            'title' => "S'amuser",
+            'description' => 'Après tout, nous sommes tous en train de jouer à un jeu vidéo, alors essayons de passer du bon temps ensemble pour avoir une expérience en ligne inoubliable ! 😉',
         ],
     ],
 
-    "rules" => [
-        "title" => "Règles",
-        "description" => [
-            "1" => "La section suivante décrit les différents ",
-            "2" => "comportements interdits",
-            "3" => " sur le serveur RedCraft.org. Ces règles s'appliquent à tous les joueurs, qu'ils soient membres du staff ou non.",
+    'rules' => [
+        'title' => 'Règles',
+        'description' => [
+            '1' => 'La section suivante décrit les différents ',
+            '2' => 'comportements interdits',
+            '3' => " sur le serveur RedCraft.org. Ces règles s'appliquent à tous les joueurs, qu'ils soient membres du staff ou non.",
         ],
-        "note" => "Remarque : ",
-        "general" => [
-            "title" => 'Général',
-            "1" => [
-                "title" => "Comportement général",
-                "1" => "L'usurpation d'identité.",
-                "2" => "Avoir un pseudonyme, un nom ou une photo de profil outrageant.",
-                "3" => "Tout comportement portant atteinte à l'intégrité d'une personne ou d'un groupe de personnes (insultes, provocation, discrimination, harcèlement, homophobie, transphobie), par message textuel, par discussion vocale, par réaction avec des emojis ou par tout autre moyen de communication.",
-                "4" => "Le spam des salons textuels, vocaux et des mentions au staff.",
-                "5" => "L'utilisation du langage SMS dans les canaux publics.",
-                "6" => "La divulgation d'informations privées.",
+        'note' => 'Remarque : ',
+        'general' => [
+            'title' => 'Général',
+            '1' => [
+                'title' => 'Comportement général',
+                '1' => "L'usurpation d'identité.",
+                '2' => 'Avoir un pseudonyme, un nom ou une photo de profil outrageant.',
+                '3' => "Tout comportement portant atteinte à l'intégrité d'une personne ou d'un groupe de personnes (insultes, provocation, discrimination, harcèlement, homophobie, transphobie), par message textuel, par discussion vocale, par réaction avec des emojis ou par tout autre moyen de communication.",
+                '4' => 'Le spam des salons textuels, vocaux et des mentions au staff.',
+                '5' => "L'utilisation du langage SMS dans les canaux publics.",
+                '6' => "La divulgation d'informations privées.",
             ],
-            "2" => [
-                "title" => "Le serveur Discord",
-                "1" => "L'esquive de sanctions en quittant le discord.",
-                "2" => "La publicité sur les canaux publics ainsi que la publicité massive via les canaux privés.",
-
+            '2' => [
+                'title' => 'Le serveur Discord',
+                '1' => "L'esquive de sanctions en quittant le discord.",
+                '2' => 'La publicité sur les canaux publics ainsi que la publicité massive via les canaux privés.',
             ],
         ],
         'minecraft' => [
-            "title" => "Minecraft",
-            "1" => [
-                "title" => "Général",
-                "1" => "Le grief, c'est-à-dire la destruction d'une construction d'un autre joueur sans son accord, la mise en place de pièges visant un autre joueur ou encore le vol d'items.",
-                "2" => "L'utilisation de cheats, c'est-à-dire des logiciels ou des mods, ou l'exploitation de bugs présents dans le jeu et pouvant procurer un avantage conséquent par rapport aux autres joueurs.",
-                "3" => "L'utilisation de plus d'un compte Minecraft par joueur.",
-                "4" => "L'utilisation de logiciels ou de mods destinés à récupérer ou télécharger partiellement ou entièrement la map du serveur.",
-                "5" => [
-                    "title" => "La possession continue d'un item modifié",
-                    "1" => "Donnant au joueur un avantage par rapport aux autres (effet, potion).",
-                    "2" => "Dont le nom ou la description enfreint la règle 1.3.",
-                    "3" => [
-                        "title" => "Donnant accès à des commandes auxquelles le joueur n'a normalement pas accès.",
-                        "note" => "si un joueur reçoit ou trouve un item modifié tel que décrit par la règle 2.1.4, il doit immédiatement avertir le staff, donner l'item à un membre du staff et s'en débarrasser par la suite.",
+            'title' => 'Minecraft',
+            '1' => [
+                'title' => 'Général',
+                '1' => "Le grief, c'est-à-dire la destruction d'une construction d'un autre joueur sans son accord, la mise en place de pièges visant un autre joueur ou encore le vol d'items.",
+                '2' => "L'utilisation de cheats, c'est-à-dire des logiciels ou des mods, ou l'exploitation de bugs présents dans le jeu et pouvant procurer un avantage conséquent par rapport aux autres joueurs.",
+                '3' => "L'utilisation de plus d'un compte Minecraft par joueur.",
+                '4' => "L'utilisation de logiciels ou de mods destinés à récupérer ou télécharger partiellement ou entièrement la map du serveur.",
+                '5' => [
+                    'title' => "La possession continue d'un item modifié",
+                    '1' => 'Donnant au joueur un avantage par rapport aux autres (effet, potion).',
+                    '2' => 'Dont le nom ou la description enfreint la règle 1.3.',
+                    '3' => [
+                        'title' => "Donnant accès à des commandes auxquelles le joueur n'a normalement pas accès.",
+                        'note' => "si un joueur reçoit ou trouve un item modifié tel que décrit par la règle 2.1.4, il doit immédiatement avertir le staff, donner l'item à un membre du staff et s'en débarrasser par la suite.",
                     ],
                 ],
             ],
-            "2" => [
-                "title" => "Créatif Redstone",
-                "1" => [
-                    "title" => "La création de clocks, c'est-à-dire des systèmes provoquant une activation répétée du système sans interaction nécessaire de la part du joueur.",
-                    "note" => "les clocks s'arrêtant automatiquement au bout d'un court instant sont tolérées tant qu'elles sont réactivables uniquement via l'interaction d'un joueur.",
+            '2' => [
+                'title' => 'Créatif Redstone',
+                '1' => [
+                    'title' => "La création de clocks, c'est-à-dire des systèmes provoquant une activation répétée du système sans interaction nécessaire de la part du joueur.",
+                    'note' => "les clocks s'arrêtant automatiquement au bout d'un court instant sont tolérées tant qu'elles sont réactivables uniquement via l'interaction d'un joueur.",
                 ],
-                "2" => "Le spam des systèmes redstone des autres joueurs.",
-                "3" => "L'appropriation d'une création qui n'a pas été créée par soi-même.",
+                '2' => 'Le spam des systèmes redstone des autres joueurs.',
+                '3' => "L'appropriation d'une création qui n'a pas été créée par soi-même.",
             ],
-            "3" => [
-                "title" => "Créatif Build",
-                "1" => "Les règles de la section 2.1 s'appliquent ici.",
+            '3' => [
+                'title' => 'Créatif Build',
+                '1' => "Les règles de la section 2.1 s'appliquent ici.",
+            ],
+            '4' => [
+                'title' => 'Survie',
+                '1' => 'Collaboration: Pour contribuer à un projet public, il faut d’abord se référer au joueur responsable de la construction. Si vous souhaitez entreprendre un grand projet, assurez-vous de ne pas déranger d’autres joueurs qui construisent à proximité. Dans le doute, demandez.',
+                '2' => 'Toutes les fermes doivent pouvoir être désactivées afin d’éviter le lag.',
+                '3' => [
+                    'title' => 'L’abus de mécaniques non-officielles',
+                    'description' => 'telle que les bugs et les glitches qui procurent un avantage déloyal par rapport aux autres joueurs est interdite.',
+                    'note' => [
+                        'title' => 'Remarque',
+                        'table' => [
+                            'allowed' => 'Autorisé',
+                            'not_allowed' => 'Interdit',
+                            'examples' => [['Duplication de TNT Exemple : machine à tunnel, quarry', 'Duplication de coffre, de shulker et de tout autres conteneurs'], ['FreeCam', 'Minage avec Xray ou en utilisant un mod FreeCam'], ['Autoclicker', 'Avoir un Mod donnant un avantage sur le PVP, par exemple Kill Aura ou Kill Reach'], ['', 'Minage automatique. Par exemple : Baritone']],
+                        ],
+                    ],
+                    'note_2temp' => 'Remarque : Les clients modifiés (mods) sont des mécaniques non officielles.',
+                ],
+                '4' => [
+                    'title' => 'Ne détruisez pas les constructions ou les biens des autres joueurs sans leur permission.',
+                    'description' => 'Respectez le travail et les efforts des autres. Ne détruisez pas ou ne modifiez pas leurs bâtiments, leurs cultures, leurs élevages ou toute autre structure. Pour toute structure abandonnée par un joueur inactif, l’autorisation devra être demandée pour utiliser l’espace.',
+                    'note' => 'Avant de modifier le terrain ou d’emprunter les affaires d’une personne, assurez-vous d’avoir son accord. Dans le doute, demandez !',
+                ],
+                '5' => 'Voler les affaires ou les ressources d’un autre joueur est interdit.',
+                '6' => "Le kill farming, c'est-à-dire la tuerie répétée d'autres joueurs est interdit.",
             ],
         ],
     ],
--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -38,6 +38,28 @@
             __('rules.rules.minecraft.3.title') => [
                 __('rules.rules.minecraft.3.1'),
             ],
+            __('rules.rules.minecraft.4.title') => [
+                __('rules.rules.minecraft.4.1'),
+                __('rules.rules.minecraft.4.2'),
+                __('rules.rules.minecraft.4.3.title') => [
+                    'description' => __('rules.rules.minecraft.4.3.description'),
+                    'note' => [
+                        'title' => __('rules.rules.minecraft.4.3.note.title'),
+                        'table' => [
+                            'allowed' => __('rules.rules.minecraft.4.3.note.table.allowed'),
+                            'not_allowed' => __('rules.rules.minecraft.4.3.note.table.not_allowed'),
+                            'examples' => __('rules.rules.minecraft.4.3.note.table.examples'), // This assumes you have added the examples as an array in your language file.
+                        ],
+                    ],
+                    'note_2temp' => __('rules.rules.minecraft.4.3.note_2temp'),
+                ],
+                __('rules.rules.minecraft.4.4.title') => [
+                    'description' => __('rules.rules.minecraft.4.4.description'),
+                    'note' => __('rules.rules.minecraft.4.4.note'),
+                ],
+                __('rules.rules.minecraft.4.5'),
+                __('rules.rules.minecraft.4.6'),
+            ],
         ],
     ];
 @endphp
@@ -48,7 +70,7 @@
 
     <x-section section-title="{{ __('rules.code_of_conduct.title') }}" bg="bg-base-100" text="text-light" wave-bg="fill-base-100" wave-id="3"
         x-data="{ open: 'respect' }">
-        <div class="flex flex-col md:flex-row justify-around my-4">
+        <div class="flex flex-col justify-around my-4 md:flex-row">
             <div class="code-conduct-item" tabindex="0" @mouseover="open = 'respect'"
                 :class="open == 'respect' && 'focused'">
                 <i class="fas fa-hands-helping code-conduct-icon"></i>
@@ -69,13 +91,13 @@
             {{-- Note: remove the `absolute w-full top-0` classes on the element with the longest text --}}
             {{--       in order to have the height of the container relative to the longest child --}}
             <div class="code-conduct-details" :class="open != 'respect' && 'opacity-0'">
-                <div class="text text-xl">@lang('rules.code_of_conduct.1.description')</div>
+                <div class="text-xl text">@lang('rules.code_of_conduct.1.description')</div>
             </div>
-            <div class="code-conduct-details absolute w-full top-0" :class="open != 'mutualaid' && 'opacity-0'">
-                <div class="text text-xl">@lang('rules.code_of_conduct.2.description')</div>
+            <div class="absolute top-0 w-full code-conduct-details" :class="open != 'mutualaid' && 'opacity-0'">
+                <div class="text-xl text">@lang('rules.code_of_conduct.2.description')</div>
             </div>
-            <div class="code-conduct-details absolute w-full top-0" :class="open != 'havefun' && 'opacity-0'">
-                <div class="text text-xl">@lang('rules.code_of_conduct.3.description')</div>
+            <div class="absolute top-0 w-full code-conduct-details" :class="open != 'havefun' && 'opacity-0'">
+                <div class="text-xl text">@lang('rules.code_of_conduct.3.description')</div>
             </div>
         </div>
     </x-section>
@@ -85,7 +107,7 @@
 
         {{-- Wall of Shame card --}}
         {{-- Remove these comments when adding the "Wall of Shame" page --}}
-        {{-- <div class="card-wall-of-shame card border-dark float-md-right mx-md-3 my-3 col-12 col-md-6 col-lg-4"> --}}
+        {{-- <div class="my-3 card-wall-of-shame card border-dark float-md-right mx-md-3 col-12 col-md-6 col-lg-4"> --}}
         {{-- <div class="card-body"> --}}
         {{-- <h5 class="card-title">Le Mur de la Honte</h5> --}}
         {{-- <p class="card-text">Sur cette page se trouve la liste de toutes les personnes actuellement sanctionnées sur RedCraft.org, la --}}
@@ -96,7 +118,7 @@
 
         <em>
             @lang('rules.rules.description.1')
-            <b class="text-primary not-italic">@lang('rules.rules.description.2')</b>
+            <b class="not-italic text-primary">@lang('rules.rules.description.2')</b>
             @lang('rules.rules.description.3')
         </em>
         <div class="my-3">
