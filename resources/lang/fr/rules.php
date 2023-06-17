8e0773bac680d2cb5313b5dede81048363c512fd
Louis-Philemon Lavaud <louisphilemon.lavaud@gmail.com>
2023-06-18 00:00:14 +0100

    Revert "Tweaked rules FR"
    This reverts commit 6149068ec6e292c6979f935812ea64355dc9a050.
    
    revert 6149068

--- resources/lang/fr/rules.php
+++ resources/lang/fr/rules.php
@@ -23,7 +23,7 @@ return [
         'title' => 'Règles',
         'description' => [
             '1' => 'La section suivante décrit les différents ',
-            '2' => 'comportements à suivre',
+            '2' => 'comportements interdits',
             '3' => " sur le serveur RedCraft.org. Ces règles s'appliquent à tous les joueurs, qu'ils soient membres du staff ou non.",
         ],
         'note' => 'Remarque : ',
@@ -31,31 +31,29 @@ return [
             'title' => 'Général',
             '1' => [
                 'title' => 'Comportement général',
-                '1' => "L'usurpation d'identité est strictement interdite.",
-                '2' => 'Ayez un pseudonyme, un nom ou une photo de profil qui ne soit pas outrageant.',
-                '3' => "Tout comportement portant atteinte à l'intégrité d'une personne ou d'un groupe de personnes (insultes, provocation, discrimination, harcèlement, homophobie, transphobie), par message textuel, par discussion vocale, par réaction avec des emojis ou par tout autre moyen de communication est strictement interdit.",
-                '4' => 'Ne spammez pas les salons textuels, vocaux et des mentions au staff.',
-                '5' => "Écrivez vos messages d’une façon claire et lisible. N’utilisez pas le langage SMS dans les canaux publics.",
-                '6' => "La divulgation d'informations privées est strictement interdite.",
+                '1' => "L'usurpation d'identité.",
+                '2' => 'Avoir un pseudonyme, un nom ou une photo de profil outrageant.',
+                '3' => "Tout comportement portant atteinte à l'intégrité d'une personne ou d'un groupe de personnes (insultes, provocation, discrimination, harcèlement, homophobie, transphobie), par message textuel, par discussion vocale, par réaction avec des emojis ou par tout autre moyen de communication.",
+                '4' => 'Le spam des salons textuels, vocaux et des mentions au staff.',
+                '5' => "L'utilisation du langage SMS dans les canaux publics.",
+                '6' => "La divulgation d'informations privées.",
             ],
             '2' => [
                 'title' => 'Le serveur Discord',
-                '1' => "N’esquivez pas les sanctions en quittant le discord.",
-                '2' => 'Gardez la discussion propre, ne faites pas de publicité sur les canaux publics ainsi que de la publicité via les canaux privés.',
+                '1' => "L'esquive de sanctions en quittant le discord.",
+                '2' => 'La publicité sur les canaux publics ainsi que la publicité massive via les canaux privés.',
             ],
         ],
         'minecraft' => [
             'title' => 'Minecraft',
             '1' => [
                 'title' => 'Général',
-                '1' => [
-                    'title' => "Le grief est interdit. Cela comprends la destruction des constructions d’un autre joueur sans son accord, la mise en place de pièges visant un autre joueur ou encore le vol d'items.",
-                    'note' => "Être membre du plot d’un autre joueur ne signifie pas que vous pouvez modifier son terrain sans son accord.",
-                ],
-                '2' => "L'utilisation de cheats, c'est-à-dire des logiciels ou des mods, ou l'exploitation de bugs présents dans le jeu et pouvant procurer un avantage déloyal par rapport aux autres joueurs est interdite.",
-                '3' => "N’utilisez pas de logiciels ou de mods destinés à récupérer ou télécharger partiellement ou entièrement la map du serveur.",
-                '4' => [
-                    'title' => "La possession continue d'un item modifié est interdite. Cela inclut les items :",
+                '1' => "Le grief, c'est-à-dire la destruction d'une construction d'un autre joueur sans son accord, la mise en place de pièges visant un autre joueur ou encore le vol d'items.",
+                '2' => "L'utilisation de cheats, c'est-à-dire des logiciels ou des mods, ou l'exploitation de bugs présents dans le jeu et pouvant procurer un avantage conséquent par rapport aux autres joueurs.",
+                '3' => "L'utilisation de plus d'un compte Minecraft par joueur.",
+                '4' => "L'utilisation de logiciels ou de mods destinés à récupérer ou télécharger partiellement ou entièrement la map du serveur.",
+                '5' => [
+                    'title' => "La possession continue d'un item modifié",
                     '1' => 'Donnant au joueur un avantage par rapport aux autres (effet, potion).',
                     '2' => 'Dont le nom ou la description enfreint la règle 1.3.',
                     '3' => [
@@ -67,11 +65,11 @@ return [
             '2' => [
                 'title' => 'Créatif Redstone',
                 '1' => [
-                    'title' => "La création de clocks, c'est-à-dire des systèmes provoquant une activation répétée du système sans interaction nécessaire de la part du joueur est interdite.",
+                    'title' => "La création de clocks, c'est-à-dire des systèmes provoquant une activation répétée du système sans interaction nécessaire de la part du joueur.",
                     'note' => "les clocks s'arrêtant automatiquement au bout d'un court instant sont tolérées tant qu'elles sont réactivables uniquement via l'interaction d'un joueur.",
                 ],
-                '2' => 'Ne spammez pas les systèmes redstone des autres joueurs.',
-                '3' => "Ne vous appropriez pas les créations qui n'ont pas été créées par vous-même.",
+                '2' => 'Le spam des systèmes redstone des autres joueurs.',
+                '3' => "L'appropriation d'une création qui n'a pas été créée par soi-même.",
             ],
             '3' => [
                 'title' => 'Créatif Build',
@@ -98,7 +96,7 @@ return [
                     'note' => 'Avant de modifier le terrain ou d’emprunter les affaires d’une personne, assurez-vous d’avoir son accord. Dans le doute, demandez !',
                 ],
                 '5' => 'Voler les affaires ou les ressources d’un autre joueur est interdit.',
-                '6' => "Le PVP non consenti est interdit.",
+                '6' => "Le PVP non consenti est interdit. Ou tout autre form d'harcèlement.",
             ],
         ],
     ],
